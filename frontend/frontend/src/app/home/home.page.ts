b71cb01485c85fe8309141bb85420672e87d9b30
marthamwangi <martyturing@gmail.com>
2024-05-07 13:31:55 +0300

    chore: activate route for navigating to detail component

--- frontend/frontend/src/app/home/home.page.ts
+++ frontend/frontend/src/app/home/home.page.ts
@@ -4,18 +4,24 @@ import { IonicModule } from '@ionic/angular';
 import { BingwaService } from '../services/bw-service/data.service';
 import { CurrencyPipe, NgFor } from '@angular/common';
 import { IBwService } from '../services/bw-service/data.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FilterMenuComponent } from './section/filter-menu/filter-menu.component';
 
 @Component({
   selector: 'bt-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule, NgFor, CurrencyPipe],
+  imports: [IonicModule, NgFor, CurrencyPipe, FilterMenuComponent],
 })
 export class HomePage implements OnInit {
   #bwServices = inject(BingwaService);
+  #router: Router = inject(Router);
+  #activatedRouter: ActivatedRoute = inject(ActivatedRoute);
+
   bwServices: Array<IBwService> = [];
   bookicon = 'assets/icon/solar_home-add-bold-duotone.svg';
+  filtericon = 'assets/icon/lets-icons_filter-alt-duotone-line.svg';
 
   ngOnInit(): void {
     this._getServices();
@@ -28,8 +34,11 @@ export class HomePage implements OnInit {
       .subscribe({
         next: (data) => {
           this.bwServices = data;
-          console.log('bwServices', this.#bwServices);
         },
       });
+  }
+
+  onSeviceClick(id: string) {
+    this.#router.navigate([id], { relativeTo: this.#activatedRouter });
   }
 }
