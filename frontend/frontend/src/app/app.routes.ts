7ba0de908cd004d932719d5b750e53b0bf270fdc
marthamwangi <martyturing@gmail.com>
2024-05-07 13:30:18 +0300

    new: component for service detail

--- frontend/frontend/src/app/app.routes.ts
+++ frontend/frontend/src/app/app.routes.ts
@@ -2,12 +2,19 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
-    path: 'home',
+    path: '',
     loadComponent: () => import('./home/home.page').then((c) => c.HomePage),
   },
   {
+    path: ':id',
+    loadComponent: () =>
+      import('./detail-view/detail-view.component').then(
+        (c) => c.DetailViewComponent
+      ),
+  },
+  {
     path: '',
-    redirectTo: 'home',
+    redirectTo: '',
     pathMatch: 'full',
   },
 ];
---
+++ frontend/frontend/src/app/detail-view/detail-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'bw-detail-view',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './detail-view.component.html',
+  styleUrl: './detail-view.component.css',
+})
+export class DetailViewComponent {}
