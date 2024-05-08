4eae30d13182425dd29749e68166a5a6f9706a1d
marthamwangi <martyturing@gmail.com>
2024-05-08 13:22:50 +0300

    chore: append service_provider to the services api

--- backend/backend/src/app/bingwa-services/bw-service.service.ts
+++ backend/backend/src/app/bingwa-services/bw-service.service.ts
@@ -2,16 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { BingwaService } from './bw-service.schema';
 import { Model } from 'mongoose';
-import {
-  PROVIDERS_COLLECTION_NAME,
-  SERVICES_COLLECTION_NAME,
-} from '../constants';
-import { BingwaProvider } from '../bingwa-providers/bw-provider.schema';
-
-export interface IPriceRange {
-  maxPrice: number;
-  minPrice: number;
-}
+import { SERVICES_COLLECTION_NAME } from '../constants';
 @Injectable()
 export class BingwaServicesService {
   constructor(
@@ -24,7 +15,20 @@ export class BingwaServicesService {
    * @returns all services
    */
   async findAll(): Promise<BingwaService[]> {
-    return this.bwServiceModel.find().exec();
+    // return this.bwServiceModel.find().exec();
+    try {
+      let data = await this.bwServiceModel
+        .find()
+        .populate({
+          path: 'service_provider_id',
+          select:
+            'first_name last_name email_address profile_photo phone_number location',
+        })
+        .exec();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
   }
   /**
    * findOne service and populate with the service provider
@@ -46,19 +50,4 @@ export class BingwaServicesService {
       console.log(error);
     }
   }
-
-  // async filterAll(proximity: IProximity, price: IPriceRange, service: string) {
-  //   return this.bwServiceModel.find({
-  //     location: {
-  //       $near: {
-  //         $geometry: {
-  //           type: 'Point',
-  //           coordinates: [proximity.long, proximity.lat],
-  //         },
-  //         $minDistance: proximity.minDistance,
-  //         $maxDistance: proximity.maxDistance,
-  //       },
-  //     },
-  //   });
-  // }
 }
