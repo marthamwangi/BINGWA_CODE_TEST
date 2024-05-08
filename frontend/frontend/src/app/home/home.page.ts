f7bf0aa71fcb828085b3a291ce9fd855a8d865b4
marthamwangi <martyturing@gmail.com>
2024-05-08 13:27:09 +0300

    chore:show providers locationa

--- frontend/frontend/src/app/home/home.page.ts
+++ frontend/frontend/src/app/home/home.page.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
-
-import { BingwaService } from '../services/bw-service/data.service';
 import { CurrencyPipe, NgFor } from '@angular/common';
 import { IBwService } from '../services/bw-service/data.model';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -15,10 +13,8 @@ import { FilterMenuComponent } from './section/filter-menu/filter-menu.component
   imports: [IonicModule, NgFor, CurrencyPipe, FilterMenuComponent],
 })
 export class HomePage {
-  // #bwServices = inject(BingwaService);
   #router: Router = inject(Router);
   #activatedRouter: ActivatedRoute = inject(ActivatedRoute);
-
   bwServices: Array<IBwService> = [];
   bookicon = 'assets/icon/solar_home-add-bold-duotone.svg';
   filtericon = 'assets/icon/lets-icons_filter-alt-duotone-line.svg';
