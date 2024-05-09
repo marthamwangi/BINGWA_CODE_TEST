16a60e787ae160a1cb765b73f4ce24e1d785dbc5
marthamwangi <martyturing@gmail.com>
2024-05-09 09:53:01 +0300

    chore: refactor api request urls

--- frontend/frontend/src/app/constants/constants.ts
+++ frontend/frontend/src/app/constants/constants.ts
@@ -1,1 +1,4 @@
 export const LOCATION_STORAGE_KEY = 'bw.coordinates';
+export const SERVICE_TYPES_URL = 'http://localhost:3000/api/servicetypes';
+export const SERVICES_URL = 'http://localhost:3000/api/services';
+export const BOOK_SERVICE = `${SERVICES_URL}/booking`;
