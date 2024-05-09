bcebc87c7c18554dc6b96ae1b74dc7131ca8b797
marthamwangi <martyturing@gmail.com>
2024-05-09 09:52:35 +0300

    chore: show modal and toast

--- frontend/frontend/src/app/home/home.page.ts
+++ frontend/frontend/src/app/home/home.page.ts
@@ -1,28 +1,49 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
-import { CurrencyPipe, NgFor } from '@angular/common';
+import { CurrencyPipe, NgFor, NgIf } from '@angular/common';
 import { IBwService } from '../services/bw-service/data.model';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { FilterMenuComponent } from './section/filter-menu/filter-menu.component';
+import { BookingComponent } from '../booking/booking.component';
 
 @Component({
   selector: 'bt-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule, NgFor, CurrencyPipe, FilterMenuComponent],
+  imports: [
+    IonicModule,
+    NgFor,
+    CurrencyPipe,
+    FilterMenuComponent,
+    BookingComponent,
+    NgIf,
+  ],
 })
 export class HomePage {
   #router: Router = inject(Router);
-  #activatedRouter: ActivatedRoute = inject(ActivatedRoute);
   bwServices: Array<IBwService> = [];
   bookicon = 'assets/icon/solar_home-add-bold-duotone.svg';
   filtericon = 'assets/icon/lets-icons_filter-alt-duotone-line.svg';
+  showToast$: boolean = false;
+  showToastMessage$: string = 'empty';
+  public serviceId: string = '';
 
   onLoadServices($event: any) {
     this.bwServices = $event;
   }
   onSeviceClick(id: string) {
-    this.#router.navigate([id], { relativeTo: this.#activatedRouter });
+    this.#router.navigate([id]);
+  }
+
+  onBooking($clickEvent: MouseEvent, id: string) {
+    $clickEvent.stopImmediatePropagation();
+    $clickEvent.preventDefault();
+    this.serviceId = id;
+  }
+
+  toggleToast($event: any) {
+    this.showToast$ = $event.show;
+    this.showToastMessage$ = $event.message;
   }
 }
